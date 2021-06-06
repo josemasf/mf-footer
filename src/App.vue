764dc7bafc30b8c3482021d71299e1e882f6a66f
josemasf <josemasf@gmail.com>
2021-06-06 14:12:09 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div id="vue-footer-app">    
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,7 +16,7 @@ export default {
 </script>
 
 <style>
-#app {
+#vue-footer-app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
